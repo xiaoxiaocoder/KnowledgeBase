43ba3a497a458318b91a2a9cba26ba452fdb6236
zhaoyan <zhaoyan1@yingu.com>
2017-12-17 00:31:12 +0800

    es6 learning notes

---
+++ es6Learning/src/01 简介/babel-core-demo.js
@@ -0,0 +1,21 @@
+var babel = require('babel-core');
+
+
+// http://babeljs.io/docs/usage/api/
+
+// 字符串转码
+babel.transform('code();', options);
+// => { code, map, ast }
+
+// 文件转码（异步）
+babel.transformFile('filename.js', options, function(err, result) {
+  result; // => { code, map, ast }
+});
+
+// 文件转码（同步）
+babel.transformFileSync('filename.js', options);
+// => { code, map, ast }
+
+// Babel AST转码
+babel.transformFromAst(ast, code, options);
+// => { code, map, ast }
